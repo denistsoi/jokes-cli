3a5d762d9bc6815ec1e6c3a7f32d6e944f938724
Denis Tsoi <denistsoi@gmail.com>
2017-06-27 13:03:57 +0800

    merge branch v7; update to 1.1.0;

---
+++ lib.js
@@ -0,0 +1,73 @@
+#!/usr/bin/env node
+const https = require('https');
+const color = require('colors');
+
+const source = {
+  reddit: {
+    url: 'https://www.reddit.com',
+    sub: {
+      dad: '/r/dadjokes.json'
+    }
+  },
+}
+
+function filterOutLink(j) {
+  return j.data.selftext.indexOf("http") == -1;
+}
+function filterUpVotes(j) {
+  return j.data.ups >= 10;
+}
+
+async function fetch() {
+  return new Promise((resolve, reject) => {
+    let req = https.get(source.reddit.url + source.reddit.sub.dad, (response)=> {
+      let json = '';
+      response.on('data', (chunk)=>{
+        json += chunk;
+      });
+
+      response.on('end', ()=> {
+        try {
+          var body = json;
+        } catch (e) {
+          reject(e);
+        }
+        resolve(body);
+      });
+      
+      response.on('error', (err)=>{
+        reject(e);
+        console.warn(err);
+      }); 
+    });
+
+  });
+}
+
+const outputResponse = function (body) {
+  const jokes = JSON.parse(body).data.children
+    .filter(filterOutLink)
+    .filter(filterUpVotes)
+    .map((joke)=>{
+      let { title, selftext } = joke.data;
+      return { title, selftext };
+    });
+
+  let result = [];
+  jokes.slice(0,3).map((joke, i)=>{
+    joke = {
+      title: 'Joke ' + (i+1) + ':',
+      punchline: joke.title + '\n' + joke.selftext + '\n'
+    }
+    result.push(joke);
+  });
+
+  return result;
+};
+
+module.exports = new Promise((res, rej)=> {
+  fetch()
+    .then((body) => {
+      res(outputResponse(body));
+    });
+});
